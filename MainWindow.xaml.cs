ca9ad998badf14b8a6775dbd5ce5acca5756beaf
Ben Hunter <57240557+megabytesme@users.noreply.github.com>
2024-10-20 21:38:16 +0100

    Implemented methods to upload files to file.io and set the progress ring to current file upload progress

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -6,6 +6,13 @@ using Windows.Storage.Pickers;
 using Windows.Storage;
 using Microsoft.UI.Xaml.Media;
 using System.Threading.Tasks;
+using Windows.Storage.FileProperties;
+using System.IO;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Net;
+using Microsoft.UI.Dispatching;
+using Windows.ApplicationModel.DataTransfer;
 
 namespace QuickUp
 {
@@ -31,11 +38,9 @@ namespace QuickUp
         {
             var picker = new FileOpenPicker();
             picker.ViewMode = PickerViewMode.Thumbnail;
-
             var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
             WinRT.Interop.InitializeWithWindow.Initialize(picker, hwnd);
             picker.FileTypeFilter.Add("*");
-
             var storageFile = await picker.PickSingleFileAsync();
             if (storageFile != null)
             {
@@ -66,7 +71,108 @@ namespace QuickUp
 
         private async Task HandleFile(StorageFile file)
         {
-            progressRingButton.IsEnabled = false;
+            DispatcherQueue.TryEnqueue(() =>
+            {
+                progressRingButton.IsEnabled = false;
+                progressRingButton.Content = "Uploading" + Environment.NewLine + file.Name;
+            });
+
+            BasicProperties properties = await file.GetBasicPropertiesAsync();
+            ulong fileSize = properties.Size;
+            const ulong twoGB = 2UL * 1024 * 1024 * 1024;
+
+            if (fileSize > twoGB)
+            {
+                DispatcherQueue.TryEnqueue(() =>
+                {
+                    progressRingButton.Content = "File is too big!";
+                    progressRingButton.IsEnabled = true;
+                });
+                return;
+            }
+
+            await UploadFile(file);
+        }
+
+        public class ProgressableStreamContent : HttpContent
+        {
+            private const int DefaultBufferSize = 4096;
+            private readonly Stream _content;
+            private readonly int _bufferSize;
+            private readonly Action<long, long> _progress;
+
+            public ProgressableStreamContent(Stream content, Action<long, long> progress, int bufferSize = DefaultBufferSize)
+            {
+                _content = content ?? throw new ArgumentNullException(nameof(content));
+                _progress = progress ?? throw new ArgumentNullException(nameof(progress));
+                _bufferSize = bufferSize;
+            }
+
+            protected override async Task SerializeToStreamAsync(Stream stream, TransportContext context)
+            {
+                var buffer = new byte[_bufferSize];
+                long totalBytesRead = 0;
+                int bytesRead;
+
+                while ((bytesRead = await _content.ReadAsync(buffer, 0, buffer.Length)) != 0)
+                {
+                    await stream.WriteAsync(buffer, 0, bytesRead);
+                    totalBytesRead += bytesRead;
+                    _progress(totalBytesRead, _content.Length);
+                }
+            }
+
+            protected override bool TryComputeLength(out long length)
+            {
+                length = _content.Length;
+                return true;
+            }
+        }
+
+        private async Task UploadFile(StorageFile file)
+        {
+            var uri = new Uri("https://file.io/");
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var content = new MultipartFormDataContent())
+                {
+                    var fileStream = await file.OpenReadAsync();
+                    var streamContent = new ProgressableStreamContent(fileStream.AsStreamForRead(), ReportProgress);
+                    streamContent.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                    content.Add(streamContent, "file", file.Name);
+
+                    var response = await httpClient.PostAsync(uri, content);
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    DispatcherQueue.TryEnqueue(() =>
+                    {
+                        if (response.IsSuccessStatusCode)
+                        {
+                            var responseObject = System.Text.Json.JsonDocument.Parse(responseContent).RootElement;
+                            var fileLink = responseObject.GetProperty("link").GetString();
+                            var dataPackage = new DataPackage();
+                            dataPackage.SetText(fileLink);
+                            Clipboard.SetContent(dataPackage);
+                            progressRingButton.IsEnabled = true;
+                            progressRingButton.Content = "File uploaded!";
+                        }
+                        else
+                        {
+                            progressRingButton.IsEnabled = true;
+                            progressRingButton.Content = "File failed to upload.";
+                        }
+                    });
+                }
+            }
+        }
+
+        private void ReportProgress(long bytesSent, long totalBytes)
+        {
+            double progress = (double)bytesSent / totalBytes * 100;
+            DispatcherQueue.TryEnqueue(() =>
+            {
+                progressRing.Value = progress;
+            });
         }
     }
 }
